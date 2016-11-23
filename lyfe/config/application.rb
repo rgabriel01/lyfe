3ea2c5f53f52b53f5ca06d47e353db9370eecf62
raymond <raymond@serviceseeking.com.au>
2016-11-23 16:30:54 +0800

    more config

--- lyfe/config/application.rb
+++ lyfe/config/application.rb
@@ -32,6 +32,7 @@ module Lyfe
         bootstrap.min.css
         bootstrap.min.css
     )
+    config.assets.paths << Rails.root.join('vendor', 'assets')
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
